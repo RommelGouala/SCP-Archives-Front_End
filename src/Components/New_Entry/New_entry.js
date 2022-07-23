6c57bae68dc97a1e7b1b2eb6f6853c835d3e2058
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2022-07-23 16:05:45 -0500

    Added dependencies: react-bootstrap, react-router-dom. Added code: Navbar v1 complit. New_entry, Scp (and their css) start step. Added: routers path.

---
+++ src/Components/New_Entry/New_entry.js
@@ -0,0 +1,13 @@
+// JS for the New Entry (We will build a form)
+// ToDo: For the Form -- A name, Location, Date, Image, Description, and finally Containment
+
+import Navbars from "../Navbar_f/Navbar";
+
+export default function New_entry () {
+    return (
+        <div>
+            <Navbars/>
+            <h1>New Entry Page </h1>
+        </div>
+    )
+}
---
+++ src/Components/SCP/Scp.js
@@ -0,0 +1,15 @@
+// This is where the whole list of SCP archives will be displayed.
+// IF we can categorize the list by years of discovery, that would be great.
+// We'll use cards to display each one, then when they click on a card, it should take them to another page where they'll find more in-depth information about the clicked SCP.
+
+import Navbars from "../Navbar_f/Navbar";
+
+
+export default function Scp () {
+    return (
+        <div>
+            <Navbars/>
+            <h1>SCP Page List</h1>
+        </div>
+    )
+}
