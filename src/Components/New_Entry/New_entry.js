e328b0e4966197cc0282c6e01ae827f457018a1b
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2022-07-27 00:45:43 -0500

    added some pictures, about to switch branch

--- src/Components/New_Entry/New_entry.js
+++ src/Components/New_Entry/New_entry.js
@@ -35,24 +35,24 @@ export default function New_entry () {
         }).then(response => console.log('Data Posted', response)).catch(err => console.log(err))
     }
     return (
-        <div>
+        <div className="New_Entry">
             <Navbars/>
             <h1>New Entry Page </h1>
             <section id="Section_O_NewEntry">
                 <form id="form_NE">
                     <label className="labels">id</label>
                 <input type='number' value={id} onChange={(e) => setId(e.target.value)} required></input>
-                <label className="labels">Name</label>
+                    <label className="labels">Name</label>
                 <input type='text' value={name} onChange={(e) => setName(e.target.value)} required></input>
-                <label className="labels">Location</label>
+                    <label className="labels">Location</label>
                 <input type='text'value={location} onChange={(e) => setLocation(e.target.value)}required></input>
-                <label className="labels">Data</label>
+                    <label className="labels">Data</label>
                 <input type='date' value={date} onChange={(e) => setDate(e.target.value)} required></input>
-                <label className="labels">Description</label>
+                    <label className="labels">Description</label>
                 <input type='text'  value={description} onChange={(e) => setDescription(e.target.value)} required></input>
-                <label className="labels">Containment</label>
+                    <label className="labels">Containment</label>
                 <input type='text'  value={containment} onChange={(e) => setContainment(e.target.value)} required></input>
-                <label className="labels">The Entry Image</label>
+                    <label className="labels">The Entry Image</label>
                 <input type='text'  value={image} onChange={(e) => setImage(e.target.value)} ></input>
                 <button onClick={hadndleSubmit}>Submit Entry</button>
                 </form>
