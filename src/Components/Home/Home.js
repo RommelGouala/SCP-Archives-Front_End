4898e125579b7823dcb82809f9035cfe8fdc5247
Rommel Gouala <rommelgouala@MacBook-Pro.local>
2022-07-30 17:36:37 -0500

    VERSION ONE

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
       <section className='Home_description_s'>
         <p id='Scp_description_p'>SCP Stands for Secure, Contain, and portec. <br/>The scp archives website is repelte with hudnreds of mob entries of strange objects and entities that violate natural law. the scp foundation is the organizaiton respoisble for containing them.</p>
 
+
       </section>
 
     </div>
