5879e806eb50fca750af705366c127ef12252582
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2022-07-31 15:52:22 -0500

    fixed broken feature in Home folder

--- src/Components/Home/Carousel.js
+++ src/Components/Home/Carousel.js
@@ -1,7 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import axios from 'axios';
-import './StylesCarousel.css';
+import ScpHomePicOne from '../Images/Bckg/ufo.jpg'
+import ScpHomePicTwo from '../Images/Bckg/sun.jpg'
+import ScpHomePicTh from '../Images/Bckg/texture.jpg'
+import './StylesCarousel.css'
 
 export default function CarouseL() {
     const [index, setIndex] = useState(0);
@@ -10,43 +12,33 @@ export default function CarouseL() {
         setIndex(selectedIndex);
     };
 
-    const Url = `https://scp-backend-server.herokuapp.com/scp`
-    const [scpList, setScp_List] = useState(null)
 
 
-    useEffect(() => {
-        axios.get(Url, { headers: {'Access-Control-Allow-Origin': '*'}})
-            .then(response => {
-                setScp_List(response.data)
-            })
-    }, [Url]);
 
-if(scpList){
 
     return (
         <Carousel activeIndex={index} onSelect={handleSelect} className='Carousel_container'>
             <Carousel.Item>
                 <img
                     className="d-block w-100 Carousel_Img"
-                    src={scpList[0].image}
+                    src={ScpHomePicOne}
                     alt="First slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100 Carousel_Img"
-                    src={scpList[1].image}
+                    src={ScpHomePicTwo}
                     alt="Second slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100 Carousel_Img"
-                    src={scpList[2].image}
+                    src={ScpHomePicTh}
                     alt="Third slide"
                 />
             </Carousel.Item>
         </Carousel>
     );
-}
 }
